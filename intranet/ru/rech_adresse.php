b1b6f7d8bf0feea7a1f6d3b007aafa9b5df15803
Robert Leguay <robert.leguay@meaux.fr>
2007-06-13 10:02:01 +0000

    ajout licence + pg pour recherche

--- intranet/ru/rech_adresse.php
+++ intranet/ru/rech_adresse.php
@@ -1,26 +1,43 @@
 <?php
-$Connection = mysql_connect("localhost","root","");
-mysql_select_db(plu, $Connection);
-$sql = "SELECT  ccosec,dnupla 
-FROM parcel 
-WHERE dnuvoi='$nter' AND dindic='$cpter' AND ccoriv LIKE '$code'";
+/*Copyright Ville de Meaux 2004-2007
+contributeur: jean-luc Dechamp - robert Leguay 
+sig@meaux.fr
 
-$result = mysql_query($sql) ;
+Ce logiciel est un programme informatique fournissant une interface cartographique WEB communale. 
 
-while($row = mysql_fetch_row($result))
-	{
-	$section1=$row[0];
-	$parcelle1=$row[1];
-	
-}
-mysql_close;
-if($parcelle1=="")
-{
-//print("aucune parcelle trouvée à cette adresse, nous vous conseillons de faire une recherche sur la carte. Cliquez <a href='../svg/'> ici</a>"); 
-print("aucune parcelle trouvée à cette adresse.<a href='ru.php'>retour</a>");
-}
-else
-{
-header("Location: ./ru.php?parcelle1=$parcelle1&section1=$section1");
+Ce logiciel est régi par la licence CeCILL-C soumise au droit français et
+respectant les principes de diffusion des logiciels libres. Vous pouvez
+utiliser, modifier et/ou redistribuer ce programme sous les conditions
+de la licence CeCILL-C telle que diffusée par le CEA, le CNRS et l'INRIA 
+sur le site "http://www.cecill.info".
+
+En contrepartie de l'accessibilité au code source et des droits de copie,
+de modification et de redistribution accordés par cette licence, il n'est
+offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
+seule une responsabilité restreinte pèse sur l'auteur du programme,  le
+titulaire des droits patrimoniaux et les concédants successifs.
+
+A cet égard  l'attention de l'utilisateur est attirée sur les risques
+associés au chargement,  à l'utilisation,  à la modification et/ou au
+développement et à la reproduction du logiciel par l'utilisateur étant 
+donné sa spécificité de logiciel libre, qui peut le rendre complexe à 
+manipuler et qui le réserve donc à des développeurs et des professionnels
+avertis possédant  des connaissances  informatiques approfondies.  Les
+utilisateurs sont donc invités à charger  et  tester  l'adéquation  du
+logiciel à leurs besoins dans des conditions permettant d'assurer la
+sécurité de leurs systèmes et ou de leurs données et, plus généralement, 
+à l'utiliser et l'exploiter dans les mêmes conditions de sécurité. 
+
+Le fait que vous puissiez accéder à cet en-tête signifie que vous avez 
+pris connaissance de la licence CeCILL-C, et que vous en avez accepté les 
+termes.*/
+include("../connexion/deb.php");
+$sql = "SELECT  ccosec,dnupla FROM parcel WHERE dnuvoi='$nter' AND dindic='$cpter' AND ccoriv LIKE '$code'";
+
+$result = tab_result($pgx,$sql) ;
+if(count($result)==0){
+	print("aucune parcelle trouvée à cette adresse.<a href='ru.php'>retour</a>");
+}else{
+	header("Location: ./ru.php?parcelle1=$parcelle1&section1=$section1");
 }
 ?> 
